81ef7d27dd48ee0d7050a4bd8666d6b463a1f065
kkenan26 <kkenan2420@gmail.com>
2026-05-06 22:43:59 +0300

    Correct settings path

--- BACKEND/manage.py
+++ BACKEND/manage.py
@@ -6,8 +6,8 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    # FIXED: Pointing to the correct inner settings
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'iles_project.iles_project.settings')
+    
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'iles_project.settings')
     
     try:
         from django.core.management import execute_from_command_line
